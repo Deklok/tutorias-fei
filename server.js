fb08c396569e9ebabad28eb39da7dc59492f88df
Daniel Escamilla <danix100@hotmail.com>
2019-10-05 15:45:48 -0500

    Encapsulación de scripts para probar en desarrollo
    Para probar el código, se corre con "npm run dev"

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require('express');
-const path = require('path');
+const bodyParser = require('body-parser');
 
 const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.get('/api/test', (req,res) =>{
+  res.send({ expressapi: 'test from api' });
+});
+
+// Use this code when is on production
+/* 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('*',(req,res) =>{
   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 });
+*/
 
 app.listen(5000);
 console.log('App listening on port 5000');
