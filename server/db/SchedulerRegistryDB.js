dd133cf9725530dd6a1297de3cdcde14b937248d
JosafatMurillo <josagears@hotmail.com>
2019-11-15 15:12:54 -0600

    Se agregan funciones

--- server/db/SchedulerRegistryDB.js
+++ server/db/SchedulerRegistryDB.js
@@ -1,54 +1,4 @@
-const mysql = require('mysql');
-const pool = require('./config.js');
-
-
-function getDataTutor(tutorId){
-	return new Promise((resolve, reject) => {
-		pool.query('SELECT * FROM Tutor WHERE personnelNum = ?',[tutorId],(err, results) => {
-			if(err){
-				return reject(err);
-			}else{
-				return resolve(results);
-			}
-		});
-	});
-}
-
-function getDataPupil(pupilId){
-	return new Promise((resolve, reject) => {
-		pool.query('call sp_get_pupilData(?)',[pupilId],(err, results) => {
-			if(err){
-				return reject(err);
-			}else{
-				return resolve(results);
-			}
-		});
-	});
-}
-
-function getAllSessions(idTutorship){
-	return new Promise((resolve, reject) => {
-		pool.query('call sp_get_allSessions(?)',[idTutorship],(err,results) => {
-			if(err){
-				return reject(err);
-			}else{
-				return resolve(results);
-			}
-		});
-	});
-}
-
-function getBlock(idTutorship){
-	return new Promise((resolve,reject) =>{
-		pool.query('SELECT * FROM Block WHERE idTutorship = ?',[idTutorship],(er,results) => {
-			if(err){
-				return reject(err);
-			}else{
-				return resolve(results);
-			}
-		});
-	});
-}
+const pool = require('./config');
 
 function getBlock(idCareer, idTutorship){
     return new Promise((resolve,reject) => {
@@ -115,10 +65,7 @@ function deleteTutorship(idTutorship){
 }
 
 module.exports = {
-	getDataTutor: getDataTutor,
-	getDataPupil: getDataPupil,
-	getAllSessions: getAllSessions,
-	getBlock: getBlock,
+    getBlock: getBlock,
     getTutorship: getTutorship,
     reserveSession: reserveSession,
     addSession: addSession,
